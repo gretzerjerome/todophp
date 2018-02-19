fcbae9189cfa3aa3832a4786694b6b8bc4f32dac
KevinCharlier <kevcharlier@gmail.com>
2018-02-19 16:40:41 +0100

    modif

--- formulaire.php
+++ formulaire.php
@@ -1,4 +1,24 @@
-<?php 
+<?php
+
+if(isset($_POST['submit'])){
+  $options = array( 'todo' => FILTER_SANITIZE_STRING, 'archive' => FILTER_SANITIZE_STRING, 'addtask' => FILTER_SANITIZE_STRING );
+  $result = filter_input_array(INPUT_POST, $options);
+  $checkResult =[];$todo = trim($result['todo']);
+  $archive = trim($result['archive']);
+  $addtask = trim($result['addtask']);
+  
+  if(isset($todo) AND !empty($todo) ){
+    $verif_todo = "ok"; }
+    else { $verif_todo = "pok"; }
+    
+  if(isset($archive) AND !empty($archive) ){
+    $verif_archive = "ok"; }
+    else { $verif_archive = "pok"; }
+    
+  if(isset($addtask) AND !empty($addtask) ){
+    $verif_addtask = "ok"; }
+    else { $verif_addtask = "pok"; } 
+}
 
 
 ?>
@@ -10,6 +30,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
+    <script src="todo.json"></script>
 		<title>To do</title>
 	</head>
 	<body>
@@ -34,6 +55,5 @@
         </fieldset>
       </form>
     </div>
-  <script src="todo.json"></script>
   </body>
 </html>
